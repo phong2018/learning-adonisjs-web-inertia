43d018444d24b1269ef34a2109f4ee02ea220ca3
phong.lg <phong2018@gmail.com>
2023-06-17 15:08:09 +0700

    001: add using @eidellev/inertia-adonisjs

---
+++ resources/js/Pages/Test.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Test = ({exampleProp}) => <div>Hello world, from {exampleProp}!) </div>
+
+export default Test
--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
 })
+
+Route.get('/test', async ({ inertia }) => {
+  return inertia.render('Test', { exampleProp: 'inertia' })
+})
